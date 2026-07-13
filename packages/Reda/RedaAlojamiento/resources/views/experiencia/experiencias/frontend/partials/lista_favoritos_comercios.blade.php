dcbe9ace8ac200c1e85e5f47e7e0128b23258d88
angelomarsanz <angelomarsanz@gmail.com>
2026-07-13 14:08:33 +0000

    20260713-1007-cloud

--- packages/Reda/RedaAlojamiento/resources/views/experiencia/experiencias/frontend/partials/lista_favoritos_comercios.blade.php
+++ packages/Reda/RedaAlojamiento/resources/views/experiencia/experiencias/frontend/partials/lista_favoritos_comercios.blade.php
@@ -1,27 +1,33 @@
 @forelse($favoritos as $experiencia)
     <div class="favorito-item d-flex align-items-center p-3 border-bottom">
-        <div class="favorito-img-wrapper mr-3">
-            @php
-                $foto = $experiencia->foto_portada;
-                $rutaFoto = asset('public/images/default-image.png');
-                if ($foto) {
-                    if (strpos($foto->photo, '/') !== false) {
-                        $rutaFoto = asset('public/images/experiencias/' . $foto->photo);
-                    } else {
-                        $rutaFoto = asset('public/images/experiencias/' . $experiencia->id . '/' . $foto->photo);
-                    }
-                }
-            @endphp
-            <img src="{{ $rutaFoto }}" alt="{{ $experiencia->titulo }}" class="img-fluid rounded shadow-sm">
+        {{-- Contenido: Imagen y Título alineados a la izquierda --}}
+        <div class="favorito-content-block">
+            <a href="{{ route('reda.negocios.experiencias.listado_productos_servicios', ['id' => $experiencia->id]) }}" 
+               class="text-decoration-none d-flex flex-column align-items-start">
+                <div class="favorito-img-wrapper mb-2" style="width: 100px;">
+                    @php
+                        $foto = $experiencia->foto_portada;
+                        $rutaFoto = asset('public/images/default-image.png');
+                        if ($foto) {
+                            if (strpos($foto->photo, '/') !== false) {
+                                $rutaFoto = asset('public/images/experiencias/' . $foto->photo);
+                            } else {
+                                $rutaFoto = asset('public/images/experiencias/' . $experiencia->id . '/' . $foto->photo);
+                            }
+                        }
+                    @endphp
+                    <img src="{{ $rutaFoto }}" alt="{{ $experiencia->titulo }}" class="img-fluid rounded shadow-sm" style="width: 100px; height: 70px; object-fit: cover;">
+                </div>
+                <div class="favorito-info">
+                    <h6 class="m-0 font-weight-700 text-dark" style="line-height: 1.3;">
+                        {{ $experiencia->titulo }}
+                    </h6>
+                </div>
+            </a>
         </div>
-        <div class="favorito-info flex-grow-1">
-            <h6 class="m-0 font-weight-700">
-                <a href="{{ route('reda.negocios.experiencias.listado_productos_servicios', ['id' => $experiencia->id]) }}" class="text-dark">
-                    {{ $experiencia->titulo }}
-                </a>
-            </h6>
-        </div>
-        <div class="favorito-acciones">
+
+        {{-- Botón Eliminar: Alineado a la derecha pero con margen controlado --}}
+        <div class="favorito-acciones ml-auto pl-3">
             <button class="btn btn-sm btn-outline-danger btn-toggle-favorito-comercio" 
                     data-id="{{ $experiencia->id }}" 
                     data-owner-id="{{ $experiencia->user_id }}"
